be7ca363eb401549c2a8588f2a94316b5405bb17
Chris <chriscenci@hotmail.com>
2018-10-17 18:25:22 -0500

    Changed variables
    Const was not needed

--- Assignment-THIS/saccelorometer/saccelorometer.ino
+++ Assignment-THIS/saccelorometer/saccelorometer.ino
@@ -1,22 +1,34 @@
-const int xPin = A0;
-const int yPin = A2;
-const int zPin = A1;
 
-void setup()
-{
-  Serial.begin(9600);      // sets the serial port to 9600
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+  // This makes it possible to directly connect the breakout board to the
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+  // you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
 }
 
-void loop()
-{
-  xPin = analogRead(0);       // read analog input pin 0
-  yPin = analogRead(2);       // read analog input pin 1
-  zPin = analogRead(1);       // read analog input pin 1
-  Serial.print("accelerations are x, y, z: ");
-  Serial.print(xPin, DEC);    // print the acceleration in the X axis
-  Serial.print(" ");       // prints a space between the numbers
-  Serial.print(yPin, DEC);    // print the acceleration in the Y axis
-  Serial.print(" ");       // prints a space between the numbers
-  Serial.println(zPin, DEC);  // print the acceleration in the Z axis
-  delay(100);              // wait 100ms for next reading
+void loop() {
+  // print the sensor values:
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  // delay before next reading:
+  delay(100);
 }
