a83babf8e854dde551499ca4787f44f8a8cb9d40
Chris <chriscenci@hotmail.com>
2018-10-17 18:50:36 -0500

    added button state for sound and mapped X to sound

--- Assignment-THIS/saccelorometer/saccelorometer.ino
+++ Assignment-THIS/saccelorometer/saccelorometer.ino
@@ -1,15 +1,38 @@
 
 int x, y,z;    //X-Axis, Y-Axis and Z-Axis       
 int piezoPin = 3;
-      
+#define BUTTON_PIN 2
+
+boolean buttonState =false;
+boolean prevButtonState =true;
 
 void setup() {
   // initialize the serial communications:
   Serial.begin(9600);
+   pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop()
 {
+  int buttonValue = digitalRead(BUTTON_PIN);
+
+  if(buttonValue == HIGH)
+  {
+   if(buttonState != prevButtonState)
+   {
+    delay(200);
+    buttonState = ! buttonState;
+    prevButtonState = buttonState;
+   }
+    
+  }
+  else
+  {
+    prevButtonState =!buttonState;
+  }
+
+    
+  
   //Read each pin so we can see where the values are being mapped to 
   x = analogRead(0);
   y = analogRead(1);
@@ -22,7 +45,17 @@ void loop()
   Serial.print(z,DEC);
   Serial.println(" ");
   delay(100);
+if(buttonState ==true)
+  {
+    tone(piezoPin,x,500);
 
-tone(piezoPin,1000,500);
+  }
+  else
+  {
+    analogWrite(piezoPin, 0);
+
+  }
+
+
   
 }
