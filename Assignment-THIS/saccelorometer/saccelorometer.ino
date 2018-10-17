8dcbd5e8f0b8d0a9009c04b29592d331fcba1594
Chris <chriscenci@hotmail.com>
2018-10-17 18:00:38 -0500

    Added variables and set them to read

--- Assignment-THIS/saccelorometer/saccelorometer.ino
+++ Assignment-THIS/saccelorometer/saccelorometer.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+const int xPin = A0;
+const int yPin = A2;
+const int zPin = A1;
 
+void setup()
+{
+  Serial.begin(9600);      // sets the serial port to 9600
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  xPin = analogRead(0);       // read analog input pin 0
+  yPin = analogRead(2);       // read analog input pin 1
+  zPin = analogRead(1);       // read analog input pin 1
+  Serial.print("accelerations are x, y, z: ");
+  Serial.print(xPin, DEC);    // print the acceleration in the X axis
+  Serial.print(" ");       // prints a space between the numbers
+  Serial.print(yPin, DEC);    // print the acceleration in the Y axis
+  Serial.print(" ");       // prints a space between the numbers
+  Serial.println(zPin, DEC);  // print the acceleration in the Z axis
+  delay(100);              // wait 100ms for next reading
 }
