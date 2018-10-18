b763f327c435256bb0a3a95a18d5b16883eba69c
Chris <chriscenci@hotmail.com>
2018-10-17 20:54:18 -0500

    mapped speed to location

--- Assignment-THIS/saccelorometer/saccelorometer.ino
+++ Assignment-THIS/saccelorometer/saccelorometer.ino
@@ -1,5 +1,22 @@
 
-int x, y,z;    //X-Axis, Y-Axis and Z-Axis       
+int x;//Center is 345
+int preX;
+int currentX;
+int y;//Center is 
+int preY;
+int z;   
+int preZ; 
+//X-Axis, Y-Axis and Z-Axis  
+//X = Side to side
+//Z = Up and Down
+//Y = Front and backwards
+//<345  = down
+//> 345 = up 
+
+
+int xLoc;
+int yLoc;
+int zLoc;
 int piezoPin = 3;
 #define BUTTON_PIN 2
 
@@ -14,6 +31,13 @@ void setup() {
 
 void loop()
 {
+
+  preX = 345;
+  currentX = x - preX;
+
+  if (currentX == currentX +15) {
+    currentX += analogRead(0);
+  }
   int buttonValue = digitalRead(BUTTON_PIN);
 
   if(buttonValue == HIGH)
@@ -31,31 +55,45 @@ void loop()
     prevButtonState =!buttonState;
   }
 
-    
-  
+
   //Read each pin so we can see where the values are being mapped to 
   x = analogRead(0);
   y = analogRead(1);
   z = analogRead(2);
   //Print the locations in the Serial Monitor 
-  Serial.print(x,DEC);
-  Serial.print(" ");
-  Serial.print(y,DEC);
-  Serial.print(" ");
-  Serial.print(z,DEC);
-  Serial.println(" ");
+  Serial.println(currentX,DEC);
+ // Serial.println(xLoc,DEC);
+
   delay(100);
 if(buttonState ==true)
   {
-    tone(piezoPin,x,500);
+    tone(piezoPin,x,1000);
+    delay(100);
 
   }
   else
   {
-    analogWrite(piezoPin, 0);
+    noTone(piezoPin);
 
   }
 
-
   
+}
+
+
+void mapLoc() {
+ // sound-x
+  //sound-y
+ // sound-z
+  if (x < 50) {
+//    piexoPin = 1
+  }
+  if (x >50 && x <100) {
+  
+    
+  }
+  
+
+
+
 }
