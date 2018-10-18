20d068c158969f16998cf9e11c5ff48fbbf6f11a
Chris <chriscenci@hotmail.com>
2018-10-17 23:23:11 -0500

    added second speaker, mapping for Y and Z, and working on a threshold

--- Assignment-THIS/saccelorometer/saccelorometer.ino
+++ Assignment-THIS/saccelorometer/saccelorometer.ino
@@ -2,31 +2,36 @@
 int x;//Center is 345
 int preX;
 int currentX;
-int y;//Center is 
+int y;//Center is
 int preY;
-int z;   
-int preZ; 
-//X-Axis, Y-Axis and Z-Axis  
+int currentY;
+int z;
+int preZ;
+int currentZ;
+int gravity;
+
+//X-Axis, Y-Axis and Z-Axis
 //X = Side to side
 //Z = Up and Down
 //Y = Front and backwards
 //<345  = down
-//> 345 = up 
+//> 345 = up
 
 
 int xLoc;
 int yLoc;
 int zLoc;
 int piezoPin = 3;
+int bigSpeaker = 5;
 #define BUTTON_PIN 2
 
-boolean buttonState =false;
-boolean prevButtonState =true;
+boolean buttonState = false;
+boolean prevButtonState = true;
 
 void setup() {
   // initialize the serial communications:
   Serial.begin(9600);
-   pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop()
@@ -35,64 +40,96 @@ void loop()
   preX = 345;
   currentX = x - preX;
 
-  if (currentX == currentX +15) {
+  if (currentX == currentX + 15) {
     currentX += analogRead(0);
   }
+
+  preY = 345;
+  currentY = y - preY;
+  if (currentY == currentY + 15) {
+    currentY += analogRead(1);
+  }
+  gravity = 40;
+  preZ = 337;
+  currentZ = z - preZ;
+  if (currentZ == currentZ - 15) {
+    //currentZ + gravity;
+    currentZ -= analogRead(2);
+
+  }
+
+
   int buttonValue = digitalRead(BUTTON_PIN);
 
-  if(buttonValue == HIGH)
+  if (buttonValue == HIGH)
   {
-   if(buttonState != prevButtonState)
-   {
-    delay(200);
-    buttonState = ! buttonState;
-    prevButtonState = buttonState;
-   }
-    
+    if (buttonState != prevButtonState)
+    {
+      delay(200);
+      buttonState = ! buttonState;
+      prevButtonState = buttonState;
+    }
+
   }
   else
   {
-    prevButtonState =!buttonState;
+    prevButtonState = !buttonState;
   }
 
 
-  //Read each pin so we can see where the values are being mapped to 
+  //Read each pin so we can see where the values are being mapped to
   x = analogRead(0);
   y = analogRead(1);
   z = analogRead(2);
-  //Print the locations in the Serial Monitor 
-  Serial.println(currentX,DEC);
- // Serial.println(xLoc,DEC);
+  /* //Print the locations in the Serial Monitor
+    Serial.print("x ");
+    Serial.print(currentX,DEC);*/
+  //  Serial.print("y ");
+  //    Serial.println(y,DEC);
+  Serial.print("x ");
+  Serial.println(currentX, DEC);
+  Serial.print("y ");
+  Serial.println(currentY,DEC);
+
+  // Serial.println(xLoc,DEC);
 
   delay(100);
-if(buttonState ==true)
+  if (buttonState == true)
   {
-    tone(piezoPin,x,1000);
-    delay(100);
+    if (currentX > 120 && currentX < 140) {
+      noTone(piezoPin);
+      tone(bigSpeaker, currentX * 10, 200);
+           //delay(100);
+    } else if (currentY > 137 && currentY < 157) {
+      noTone(bigSpeaker);
+      tone(piezoPin, currentY * 10, 200);
+
+
+    }
 
   }
   else
   {
     noTone(piezoPin);
-
+    noTone(bigSpeaker);
   }
 
-  
+
 }
 
 
 void mapLoc() {
- // sound-x
+  // sound-x
   //sound-y
- // sound-z
+  // sound-z
   if (x < 50) {
-//    piexoPin = 1
+    //    piexoPin = 1
   }
-  if (x >50 && x <100) {
-  
-    
+  if (x > 50 && x < 100) {
+
+
   }
-  
+
 
 
 
